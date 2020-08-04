6f457847ec5013389e1c47edaea4ea6ec1ae7dd4
lyj00912 <3122742760@qq.com>
2020-08-04 16:55:17 +0800

    created 008_TemplateWithImportAndInclude.py and some templates

---
+++ 008_TemplateWithImportAndInclude.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("008_MainTemplate.html",
+                           comments=["Wow!", "Fantastic!", "Sucks!"])
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",
+            port="80",
+            threaded=True,
+            debug=True)
+
+
